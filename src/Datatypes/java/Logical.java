0c82651786a6a44d23609386effdf291d661ea0c
psai200 <saiponnamanda25@gmail.com>
2024-11-08 10:57:50 +0530

    Java

--- src/Datatypes/java/Logical.java
+++ src/Datatypes/java/Logical.java
@@ -14,16 +14,16 @@ public static void main(String[] args) {
 		int c=e. nextInt();
 		System.out.println("enter value d:");
 		int d=e. nextInt();
-		boolean i= (a>=b && c==d);//flase
-		boolean j= (a>b && c<d);//true
+		boolean i= (a>=b && c==d);
+		boolean j= (a>b && c<d);
 		System.out.println(i);
 		System.out.println(j);
-		boolean k= (a>=b || c==d);//true
-		boolean l= (a<b || c<=d);//true
+		boolean k= (a>=b || c==d);
+		boolean l= (a<b || c<=d);
 		System.out.println(k);
 		System.out.println(l);
-		boolean m= !(a>=b );//flase
-		boolean n=! (c==d);//true
+		boolean m= !(a>=b );
+		boolean n=! (c==d);
 		System.out.println(m);
 		System.out.println(n);
 		
