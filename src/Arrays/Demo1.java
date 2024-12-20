279026953c3cc1321d93d4f580308149c91d9d56
psai200 <saiponnamanda25@gmail.com>
2024-12-20 17:08:36 +0530

    java

---
+++ src/Arrays/Demo1.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("enter the element");
+		int a=sc.nextInt();
+		int b[]=new int[a];
+		System.out.println("legth of array: "+b.length);
+		for(int i=0;i<b.length;i++) {
+		b[i]=sc.nextInt();
+		System.out.println(b[i]);
+		}
+		System.out.println("-------------"); 
+		for(int i=b.length-1 ;i>=0;i--) {
+		System.out.println(b[i]);
+		}
+	}
+
+}
