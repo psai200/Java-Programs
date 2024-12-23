0b91a1c454777a8d1f1a995d0942a44868a0d447
psai200 <saiponnamanda25@gmail.com>
2024-12-23 12:46:10 +0530

    duplicate element file

--- src/Arrays/Duplicate_element.java
+++ src/Arrays/Duplicate_element.java
@@ -7,13 +7,23 @@ public class Duplicate_element {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc =new Scanner(System.in);
-		System.out.println("enter the element:");
+		System.out.println("enter the len:");
 		int a= sc.nextInt();
-		int arr[] =new int[a];
-		for(int i=0;i>arr.length;i++) {
-	
+		System.out.println("Please enter elements here");
+		int arr[]=new int [a];
+		for(int i=0;i<a;i++) {
+			arr[i]=sc.nextInt();
+		 
+		}
+		for(int i=0;i<arr.length;i++) {
+		     for (int j=i+1;j<arr.length;j++) {
+		    	 if(arr[i]==arr[j]) {
+		    		 System.out.println(arr[i]);
+		    		 
+		    		 break;
+		    	 }	
+		     }
 		}
 		
 	}
-
 }
